3dd0cdf4d44ff54291ba305aca222f7f0e761841
spacefall <lucarichu@outlook.it>
2025-01-05 23:42:41 +0100

    feat: use cli argument to parse mkr file

--- index.ts
+++ index.ts
@@ -4,7 +4,7 @@ import { parsedToDocx } from "./src/docx.ts";
 import { parsedToHtml } from "./src/html.ts";
 import { parseString } from "./src/parse.ts";
 
-const filestr = fs.readFileSync("test2.mkr", "utf8");
+const filestr = fs.readFileSync(`${process.argv[2]}.mkr`, "utf8");
 
 const list = parseString(filestr);
 
@@ -12,8 +12,8 @@ const docx = parsedToDocx(list);
 
 const html = parsedToHtml(list);
 
-fs.writeFileSync("test.html", html);
-fs.writeFileSync("test.docx", await docx);
+fs.writeFileSync(`${process.argv[2]}.html`, html);
+fs.writeFileSync(`${process.argv[2]}.docx`, await docx);
 
 console.log("Done!");
 
