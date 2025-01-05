9c9e2e91603c5df3258ed91b11bd0b85a47a0f22
spacefall <lucarichu@outlook.it>
2025-01-05 23:33:22 +0100

    chore: can run on node.js now, bun is still used for development though

--- index.ts
+++ index.ts
@@ -1,11 +1,10 @@
+import fs from "node:fs";
 import { printList } from "./src/console.ts";
 import { parsedToDocx } from "./src/docx.ts";
 import { parsedToHtml } from "./src/html.ts";
 import { parseString } from "./src/parse.ts";
 
-const filename = "test2.mkr";
-const file = Bun.file(filename);
-const filestr = await file.text();
+const filestr = fs.readFileSync("test2.mkr", "utf8");
 
 const list = parseString(filestr);
 
@@ -13,8 +12,8 @@ const docx = parsedToDocx(list);
 
 const html = parsedToHtml(list);
 
-await Bun.write("test.html", html);
-await Bun.write("test.docx", await docx);
+fs.writeFileSync("test.html", html);
+fs.writeFileSync("test.docx", await docx);
 
 console.log("Done!");
 
